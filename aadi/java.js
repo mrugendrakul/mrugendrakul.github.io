dc975588e7c36fbae9505a043e2022139687a675
mrugendrakul <mrugendraskulkarni@gmail.com>
2023-03-05 00:59:24 +0530

    yes

---
+++ aadi/java.js
@@ -0,0 +1,10 @@
+const prjbtn= document.getElementById("prj")
+
+function menu(){
+    prjbtn.innerHTML="Pressed";
+    console.log("u pressed");
+}
+
+function addtion(){
+const add=$("#prj").addClass("bgg");
+}
